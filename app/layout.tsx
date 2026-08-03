90ac44f58724aa11d583e90c64c3bbb53a0c0834
Mei Giyanto <denmasgie0@gmail.com>
2026-08-03 17:19:19 +0000

    menambah styling pada tombol sign in & sign up

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,13 @@ export default function RootLayout({
         <ClerkProvider>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
+              <SignInButton>
+                <button className="auth-button auth-button-secondary">
+                  Sign In
+                </button>
+              </SignInButton>
               <SignUpButton>
-                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <button className="auth-button auth-button-primary">
                   Sign Up
                 </button>
               </SignUpButton>
