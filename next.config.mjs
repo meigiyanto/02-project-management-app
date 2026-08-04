a39bfe6df317c8be3dd7fce4c35cab84ae9ea983
Mei Giyanto <denmasgie0@gmail.com>
2026-08-04 04:34:22 +0000

    Menambahkan whitelist origin untuk Server Actions pada konfigurasi Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '127.0.0.1:3000', '*.app.github.dev'],
+    },
+  },
 };
 
 export default nextConfig;
